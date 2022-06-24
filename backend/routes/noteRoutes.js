07839691c9abdee43f865f36505b4302a4c4ea17
Viktor Videnov <victor.valentinov.videnov@gmail.com>
2022-06-24 11:42:26 +0300

    heroku build

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -6,7 +6,7 @@ const { getNotes, addtNote } = require('../controllers/noteController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getNotes).post(protect, addtNote)
-
+ 
 module.exports = router
 
 // Path and how ti be formated - /api/tickets/:ticketId/notes
