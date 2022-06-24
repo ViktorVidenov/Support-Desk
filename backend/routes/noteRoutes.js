fcab33d636923b3576668a1065e50986c35d1285
Viktor Videnov <victor.valentinov.videnov@gmail.com>
2022-06-24 11:54:23 +0300

    try to build

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -5,8 +5,8 @@ const { getNotes, addtNote } = require('../controllers/noteController')
 
 const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getNotes).post(protect, addtNote )
- 
-module.exports = router
+router.route('/').get(protect, getNotes).post(protect, addtNote)
 
+module.exports = router
+ 
 // Path and how ti be formated - /api/tickets/:ticketId/notes
